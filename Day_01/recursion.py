e00e51859b1370c9c8976779294b4fcc3396dc16
harshhirpara01 <harshhirpara0001@gmail.com>
2026-01-10 19:56:28 +0530

    add dff program of recursion

--- Day_01/recursion.py
+++ Day_01/recursion.py
@@ -29,3 +29,61 @@ def fun(x,n):
 fun(15,8)
 
 
+
+#functional recursion
+
+def func(sum,i,n):
+    if i > n:
+        print(sum)
+        return
+    func(sum+i,i+1,n)
+
+func(sum,1,9)
+
+
+#factorial using recursion
+
+
+def fun(n):
+    if n==0 or n ==1:
+        return 1
+    return n * fun(n-1)
+
+fun(5)
+
+
+# reverse the array
+
+def fun(nums,left,right):
+    if left >=right:
+        return
+    nums[left],nums[right] = nums[right],nums[left]
+    fun(nums,left+1,right-1)
+
+
+fun([7,98,9,9,6,6,5,8],1,8)
+
+# palindrom using recursion
+
+def fun(s,left,right):
+    if left>=right:
+        return True
+
+    if s[left] !=  s[right]:
+        return False
+
+    return fun(s,left,right-1)
+
+
+
+#fibonaaci using recursion
+
+def fibo(num):
+    if num==0 or num==1:
+        return 1
+    return fibo(num-10) + fibo(num-2)
+
+
+
+
+
