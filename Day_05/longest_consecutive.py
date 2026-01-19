b8540d7236f0ce621692110147d1bfda9f23b908
harshhirpara01 <harshhirpara0001@gmail.com>
2026-01-19 17:37:24 +0530

    add new code for longest lenth

---
+++ Day_05/longest_consecutive.py
@@ -0,0 +1,16 @@
+nums = [1,99,101,98,2,5,3,100,102]
+
+n = len(nums)
+max_count = 0
+
+for i in range(n):
+    num = nums[i]
+    count = 1
+
+    while num + 1 in nums:
+        count += 1
+        num += 1
+
+    max_count = max(max_count, count)
+
+print(max_count)
