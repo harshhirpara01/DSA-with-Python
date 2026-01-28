7f52fb2014f69f3af1d50c89122a92cc77af1883
harshhirpara01 <harshhirpara0001@gmail.com>
2026-01-28 12:50:24 +0530

    linked list and other changes

--- Day_10/single_linked_list.py
+++ Day_10/single_linked_list.py
@@ -1,46 +1,39 @@
-from numpy.ma.core import append
-from selenium.webdriver.support.expected_conditions import none_of
-
-
 class Node:
     def __init__(self,val):
-        self.val = val
+        self.val=  val
         self.next = None
 
-class singlylinkedlist:
 
+class singlylinklist:
     def __init__(self):
         self.head = None
 
 
-
     def append(self,val):
-        new_node =  Node(val)
+        new_node = Node(val)
         if self.head is None:
             self.head = new_node
         else:
             curr = self.head
             while curr.next is not None:
                 curr = curr.next
-            curr.next =  new_node
-
+            curr.next = new_node
 
     def traversal(self):
         if self.head is None:
             print("list is empty")
+
         else:
             curr = self.head
             while curr is not None:
-                print(curr.val,end=" ")
-                curr  = curr.next
+                print(curr.val, end = " ")
+                curr = curr.next
             print()
 
+all = singlylinklist()
+all.append(45)
+all.append(89)
+all.append(87)
+all.traversal()
 
-ss = singlylinkedlist()
-# ss.traversal()
-ss.append(54)
-ss.append(58)
-ss.append(96)
-
-ss.traversal()
 
