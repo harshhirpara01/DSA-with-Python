9f1268c5649348fdfb37382db273adb199c2350a
harshhirpara01 <harshhirpara0001@gmail.com>
2026-01-28 19:14:47 +0530

    middle of the linked list

--- Day_09/linked_list.py
+++ Day_09/linked_list.py
@@ -8,12 +8,51 @@ node1  = node(5)
 node2  = node(4)
 node3  = node(7)
 node4  = node(87)
+node5 = node(54)
+node6 = node(98)
 
 
-print(node1.val)
+
 
 node1.next = node2
 node2.next = node3
 node3.next = node4
+node4.next = node5
+node5.next = node6
 
-print(node1.next.next.val)
+
+
+
+print(node1.val)
+print(node2.val)
+print(node3.val)
+print(node4.val)
+print(node5.val)
+print(node6.val)
+
+head = node1
+# temp = head
+# count = 0
+# while temp is not None:
+#     temp = temp.next
+#     count +=1
+#
+#
+# lcount = 0
+# temp = head
+# while True:
+#
+#     temp = temp.next
+#     lcount +=1
+#     if lcount == count//2:
+#         print("le madar chod ",temp.val)
+#         break
+#
+slow = head
+fast = head
+
+while fast is not None and fast.next is not None:
+    slow = slow.next
+    fast = fast.next.next
+
+print(slow.val)
--- Day_10/single_linked_list.py
+++ Day_10/single_linked_list.py
@@ -1,3 +1,7 @@
+from selenium.webdriver.support.expected_conditions import none_of
+from sqlalchemy import false
+
+
 class Node:
     def __init__(self,val):
         self.val=  val
@@ -30,10 +34,60 @@ class singlylinklist:
                 curr = curr.next
             print()
 
+
+    def insert(self,position,val):
+        new_node = Node(val)
+        if position == 0:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            currunt = self.head
+            pre = None
+            count = 0
+
+            while currunt is not None and count < position:
+                pre = currunt
+                currunt = currunt.next
+                count+=1
+            pre.next = new_node
+            new_node.next = currunt
+    def delete(self,val):
+        temp = self.head
+        if temp.next is not None:
+            if temp.val == val:
+                self.head = temp.next
+                return
+            else:
+                found = False
+                prev = None
+                while temp is not None:
+                    if temp.val == val:
+                        found = True
+                        break
+                    prev = temp
+                    temp = temp.next
+
+
+                if found:
+                    prev.next = temp.next
+                    return
+                else:
+                    print("node not found")
+
+
+
+
+
+
+
+
+
+
 all = singlylinklist()
 all.append(45)
 all.append(89)
 all.append(87)
+all.append(87)
+all.append(99)
+
 all.traversal()
-
-
